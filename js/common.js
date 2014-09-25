8cf3a176eff9e26f7e8cee914bf5391d08b4ea57
Georgiy <goshazvir@gmail.com>
2014-09-25 20:31:28 +0300

    index

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 head.ready(function() {
-	console.log("These aren't the droids you're looking for!");
+	
+	function indexbg(){
+		var windowheight = $(window).height();
+		var indexbg = (windowheight - 60);
+		$(".js-page-bg-height").css('min-height', indexbg);
+	}
+	if ($(".js-page-bg-height").length > 0){
+	  indexbg();
+	}
+
+	$(window).resize(function(){
+	  if ($(".js-page-bg-height").length > 0){
+	    indexbg();
+	  }
+	});
+
+	$('.js-nav').on('click', function() {
+	 $(".nav-wrap").toggleClass("is-active");
+	});
 });
